5822513f32af2f2f89fca95804e7661b4dfcad0a
panayiotiska <panayiotiska@outlook.com>
2023-08-21 23:23:55 +0300

    Dark mode and design changes

--- src/App.js
+++ src/App.js
@@ -11,13 +11,10 @@ function App() {
     try {
       setIsLoading(true);
 
-      // Here you would make API calls to fetch risk score and other data
-      // based on walletAddress and blockchainNetwork.
-
-      // For now, let's simulate a delay to show loading state.
+      // Simulate loading...
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
-      // Simulating a random risk score for demonstration.
+      // Simulate a random risk score for demonstration.
       const randomRiskScore = Math.floor(Math.random() * 100);
       setRiskScore(randomRiskScore);
     } catch (error) {
@@ -28,35 +25,41 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Blockchain Wallet Analytics</h1>
-      <div>
-        <label>Wallet Address:</label>
-        <input
-          type="text"
-          value={walletAddress}
-          onChange={(e) => setWalletAddress(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Blockchain Network:</label>
-        <select
-          value={blockchainNetwork}
-          onChange={(e) => setBlockchainNetwork(e.target.value)}
-        >
-          <option value="Ethereum">Ethereum</option>
-          <option value="Binance">Binance</option>
-          {/* Add more options for other networks */}
-        </select>
-      </div>
-      <button onClick={handleSearch} disabled={isLoading}>
-        {isLoading ? 'Searching...' : 'Search'}
-      </button>
-      {riskScore !== null && (
-        <div>
-          <h2>Risk Score: {riskScore}</h2>
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Blockchain Wallet Analytics</h1>
+      </header>
+      <div className="app-content">
+        <div className="input-container">
+          <label htmlFor="wallet-address">Wallet Address:</label>
+          <input
+            type="text"
+            id="wallet-address"
+            value={walletAddress}
+            onChange={(e) => setWalletAddress(e.target.value)}
+          />
         </div>
-      )}
+        <div className="input-container">
+          <label htmlFor="blockchain-network">Blockchain Network:</label>
+          <select
+            id="blockchain-network"
+            value={blockchainNetwork}
+            onChange={(e) => setBlockchainNetwork(e.target.value)}
+          >
+            <option value="Ethereum">Ethereum</option>
+            <option value="Binance">Binance</option>
+            {/* Add more options for other networks */}
+          </select>
+        </div>
+        <button className="search-button" onClick={handleSearch} disabled={isLoading}>
+          {isLoading ? 'Searching...' : 'Search'}
+        </button>
+        {riskScore !== null && (
+          <div className="risk-score">
+            <h2>Risk Score: {riskScore}</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
